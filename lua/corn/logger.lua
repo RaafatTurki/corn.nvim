e6669293148d685f8d779cdd646e59643f7b5968
Raafat Turki <raafat.turki@proton.me>
2023-09-14 01:47:16 +0300

    fix: use vim.notify title opt instead of a prefix

--- lua/corn/logger.lua
+++ lua/corn/logger.lua
@@ -1,22 +1,22 @@
 local M = {}
 
-M.prefix = "[corn]"
+M.title = "Corn"
 
 function M.info(message)
   vim.schedule(function()
-    vim.notify(M.prefix .. ' ' .. message, vim.log.levels.INFO)
+    vim.notify(message, vim.log.levels.INFO, { title = M.title })
   end)
 end
 
 function M.warn(message)
   vim.schedule(function()
-    vim.notify(M.prefix .. ' ' .. message, vim.log.levels.WARN)
+    vim.notify(message, vim.log.levels.WARN, { title = M.title })
   end)
 end
 
 function M.error(message)
   vim.schedule(function()
-    vim.notify(M.prefix .. ' ' .. message, vim.log.levels.ERROR)
+    vim.notify(message, vim.log.levels.ERROR, { title = M.title })
   end)
 end
 
