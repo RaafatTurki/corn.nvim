28c2064ddaf73cd387ccafeb7392fba0be1f489c
Raafat Turki <raafat.turki@proton.me>
2024-07-04 21:58:53 +0300

    chore: remove depricated cmds

--- lua/corn.lua
+++ lua/corn.lua
@@ -33,37 +33,6 @@ M.setup = function(opts)
       })
   end
 
-  -- FIXME execute all the following after setup
-  function M.toggle(state)
-    -- on|off to true|false
-    if state ~= nil then state = state == "on" end
-    renderer.toggle(state)
-    M.render()
-  end
-
-  function M.scope(scope_type)
-    if config.opts.scope == scope_type then
-      -- do nothing
-    elseif vim.tbl_contains(scope_types, scope_type) then
-      config.opts.scope = scope_type
-      M.render()
-    else
-      logger.error("invalid scope type")
-    end
-  end
-
-  function M.scope_cycle()
-    local curr_scope_type_index = scope_types_lookup[config.opts.scope]
-    local new_scope_type_index = curr_scope_type_index + 1
-    if new_scope_type_index > #scope_types then new_scope_type_index = 1 end
-    config.opts.scope = scope_types_lookup[new_scope_type_index]
-    M.render()
-  end
-
-  function M.render()
-    renderer.render(utils.get_diagnostic_items())
-  end
-
   vim.api.nvim_create_user_command("Corn", function(opts)
     local sub_cmd = opts.fargs[1]
 
@@ -99,28 +68,37 @@ M.setup = function(opts)
         end
 
       end,
-  })
+    })
+end
 
-  -- NOTE: deprecated
-  vim.api.nvim_create_user_command("CornToggle", function(opts)
-    logger.warn("CornToggle is deprecated, use Corn toggle instead")
-    M.toggle(opts.fargs[1])
-  end, { nargs = '?' })
+function M.toggle(state)
+  -- on|off to true|false
+  if state ~= nil then state = state == "on" end
+  renderer.toggle(state)
+  M.render()
+end
 
-  vim.api.nvim_create_user_command("CornScope", function(opts)
-    logger.warn("CornScope is deprecated, use Corn scope instead")
-    M.scope(opts.fargs[1])
-  end, { nargs = 1 })
-
-  vim.api.nvim_create_user_command("CornScopeCycle", function()
-    logger.warn("CornScopeCycle is deprecated, use Corn scope_cycle instead")
-    M.scope_cycle()
-  end, {})
-
-  vim.api.nvim_create_user_command("CornRender", function()
-    logger.warn("CornRender is deprecated, use Corn render instead")
+function M.scope(scope_type)
+  if config.opts.scope == scope_type then
+    -- do nothing
+  elseif vim.tbl_contains(scope_types, scope_type) then
+    config.opts.scope = scope_type
     M.render()
-  end, {})
+  else
+    logger.error("invalid scope type")
+  end
+end
+
+function M.scope_cycle()
+  local curr_scope_type_index = scope_types_lookup[config.opts.scope]
+  local new_scope_type_index = curr_scope_type_index + 1
+  if new_scope_type_index > #scope_types then new_scope_type_index = 1 end
+  config.opts.scope = scope_types_lookup[new_scope_type_index]
+  M.render()
+end
+
+function M.render()
+  renderer.render(utils.get_diagnostic_items())
 end
 
 return M
